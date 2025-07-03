32a8f834006be1df70e133bb06ac2671d1fc1367
Rupinder Singh <46371706+jessedhammu@users.noreply.github.com>
2025-07-03 16:55:27 +0530

    First Commit

--- app/src/main/java/com/jesse/kohaapp/ui/theme/Theme.kt
+++ app/src/main/java/com/jesse/kohaapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.jesse.kohaapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
